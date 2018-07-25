e4b4e2eb27ad0717c18647a7409da732dc3f8924
rewardone <rewardone.com@gmail.com>
2018-07-25 14:57:38 -0500

    Passwords, GPO escalation, tools, varios

--- scripts/recon_enum/reconscan.py
+++ scripts/recon_enum/reconscan.py
@@ -37,6 +37,8 @@
 ##
 ## Running each script individually does not ensure their output directory paths exist...QoL feature...
 ## Fix DNSRecon
+## Expand: DirbustEverything
+##       : Photon, nice crawler. Can ensure things are not missed (currently using Cewl to crawl and wordlist)
 ## Expand: option to follow redirects in gobuster or default to follow? redirect comes at the cost of time (long time)
 ##       : But benefit of having less 301 / false negatives
 ##       : Initial testing: times are same. -r scan has more false negatives. Looks like best option will be
